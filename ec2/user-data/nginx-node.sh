7cc1f5678750a8d11d5d3b9306b50979786966c0
Paweł Kornijów <pawel.kornijow@intive.com>
2018-11-01 20:33:09 +0100

    source script for nginx proxy

--- ec2/user-data/nginx-node.sh
+++ ec2/user-data/nginx-node.sh
@@ -1,7 +1,6 @@
 # scripts and configurations
 git clone https://github.com/pawk/aws-scripts
 
-sudo apt install nginx
-sudo rm /etc/nginx/sites-enabled/default
-sudo cp ./aws-scripts/ec2/nginx-proxy.conf /etc/nginx/sites-enabled/proxy.conf
-sudo service nginx restart
+# install nginx proxy
+. ../nginx-proxy.sh
+
