eb0dab57810635bd562cce40f9831ef432375a2b
Paweł Kornijów <pawel.kornijow@intive.com>
2018-11-02 00:42:35 +0100

    ammendments

--- ec2/reverse-proxy/install.sh
+++ ec2/reverse-proxy/install.sh
@@ -1,6 +1,10 @@
 printf "installing nginx proxy...\n"
 
+workdir=$(dirname $0)
+
 sudo apt install nginx -y
-sudo rm /etc/nginx/sites-enabled/default
-sudo cp ./nginx-proxy.conf /etc/nginx/sites-enabled/proxy.conf
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+sudo cp "$workdir/nginx-proxy.conf" /etc/nginx/sites-enabled/proxy.conf
 sudo service nginx restart
