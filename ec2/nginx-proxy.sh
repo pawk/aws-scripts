82082409a194c74c0247feb7cddc4a2e06eb3baf
Paweł Kornijów <pawel.kornijow@intive.com>
2018-11-01 21:08:35 +0100

    -y

--- ec2/nginx-proxy.sh
+++ ec2/nginx-proxy.sh
@@ -2,7 +2,7 @@ printf "installing nginx proxy...\n"
 
 set -x
 
-sudo apt install nginx
+sudo apt install nginx -y
 sudo rm /etc/nginx/sites-enabled/default
 sudo cp ./nginx-proxy.conf /etc/nginx/sites-enabled/proxy.conf
 sudo service nginx restart
